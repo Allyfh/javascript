eeb79bea9233c4b8fe85f7671d723cc9f57d53a2
Allyfh Silva <phelyppallyfh864@gmail.com>
2025-02-13 10:35:41 -0300

    up

--- aula12/ex013.js
+++ aula12/ex013.js
@@ -1,6 +1,6 @@
 var agora = new Date()
 var diaSem = agora.getDay()
-
+diaSem = 0
 /*
     0 =Domingo
     1 =Segunda 
@@ -15,26 +15,26 @@ var diaSem = agora.getDay()
 switch(diaSem){
     case 0:
         console.log('Domingo')
-        
+        break
     case 1:
         console.log('Segunda')
-        
+        break
     case 2: 
         console.log('Terça')
-        
+        break
     case 3:
         console.log('Quarta') 
-        
+        break
     case 4:
         console.log('Quinta')
-        
+        break
     case 5:
         console.log('Sexta')
-       
+       break
     case 6:
         console.log('Sábado')
-          
+        break  
     default: 
         console.log('[ERROR] Dia inválido!')   
-                            
+         break                   
 }
