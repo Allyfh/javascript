34663b7d5cd1a37f80df7165ee27f7035bc6ff71
Allyfh Silva <phelyppallyfh864@gmail.com>
2025-02-13 07:46:11 -0300

    vel

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 59
+
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60){ //Condição simples
+    console.log(`Você ultrapassou o limite de velocidade. Multado!`)
+}
+console.log(`Dirija sempre respeitando o limite de ${vel}km/h`)
