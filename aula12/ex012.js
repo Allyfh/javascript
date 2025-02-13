35bcc5cda23c8b3048578a79c994fb8cdd20b3ef
Allyfh Silva <phelyppallyfh864@gmail.com>
2025-02-13 10:14:09 -0300

    date

--- aula12/ex012.js
+++ aula12/ex012.js
@@ -1,2 +1,13 @@
-var hora = 9
-console.log(`Agora são exatamente ${hora} horas.`)
+var agora = new Date()
+var hora = agora.getHours()
+var min = agora.getMinutes()
+console.log(`Agora são exatamente ${hora} horas e ${min} minutos.`)
+if (hora < 12){
+    console.log('Bom Dia!')
+} else if (hora <= 18){
+    console.log('Boa tarde!')
+} else if (hora >= 24){
+    console.log('Boa Madrugada!')
+} else {
+    console.log('Boa Noite!')
+}
