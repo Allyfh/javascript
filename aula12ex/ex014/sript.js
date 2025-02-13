5959d5372a49b90fb2f5ad8061c87f1f7eaed0c1
Allyfh Silva <phelyppallyfh864@gmail.com>
2025-02-13 12:24:51 -0300

    txt

--- aula12ex/ex014/sript.js
+++ aula12ex/ex014/sript.js
@@ -1,1 +1,20 @@
-alert ('Olá!')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var min = data.getMinutes()
+   
+    msg.innerHTML = `Agora são ${hora} horas e ${min} minutos.`
+     if (hora >= 0 && hora < 12){
+        img.src = 'man.jpg.jpg'
+        document.body.style.background = '#7DA7D1'
+     } else if (hora >=12 && hora <= 18){
+        img.src = 'tar.jpg'
+        document.body.style.background = '#FEB140'
+     } else {
+        img.src = 'noit.jpg'
+        document.body.style.background = '#1B254E'
+     }
+}
+
