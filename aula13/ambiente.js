2cb682f36c9262686045e70ea2489a6df9c6d706
Allyfh Silva <phelyppallyfh864@gmail.com>
2025-02-14 12:36:25 -0300

    do while

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,5 +1,11 @@
 var c = 1
-while (c <= 5) {
+do {
     console.log(`Candidatos: ${c}`)
     c++ //c = c + 1
-}
+} while (c <= 5)
+
+
+/*while (c <= 50) {
+    console.log(`Candidatos: ${c}`)
+    c++ //c = c + 1
+}*/
