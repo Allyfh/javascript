090b905e4eeb8fe4d249a5e1af126ea18a870e7e
Allyfh Silva <phelyppallyfh864@gmail.com>
2025-02-14 10:18:56 -0300

    finalizando interação de imagens

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -13,12 +13,40 @@ function verificar(){
        img.setAttribute('id', 'foto')
        if (fsex[0].checked){
         genero = 'Homem'
+        if (idade >=0 && idade < 10){
+            //Criança
+            img.setAttribute('src', 'crianca-h.jpg')
+        } 
+        else if (idade < 21){
+           //Jovem
+           img.setAttribute('src', 'jovem-h.jpg')
+        } else if (idade < 50){
+            //adulto
+            img.setAttribute('src', 'adult-h.jpg')
+        } else {
+            //idoso
+            img.setAttribute('src', 'idoso-h.jpg')
+        }
        } else if (fsex[1].checked){
         genero = 'Mulher'
+        if (idade >=0 && idade < 10){
+            //Criança
+            img.setAttribute('src', 'crianca-m.jpg')
+        } 
+        else if (idade < 21){
+           //Jovem
+           img.setAttribute('src', 'jovem-m.jpg')
+        } else if (idade < 50){
+            //adulto
+            img.setAttribute('src', 'adult-m.jpg')
+        } else {
+            //idoso
+            img.setAttribute('src', 'idoso-m.jpg')
+        }
        }
        res.style.textAlign = 'center'
        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
-
+       res.appendChild(img)
 
     }
 
